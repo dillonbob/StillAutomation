319342461a2547cf6028e96f9477aef44b5cbfce
Bob Dillon <bdillon@V-104-BDILLON-0.local>
2018-07-11 17:53:40 -0400

    Added ability to indicate which platform is running on (mac, pi) so that the right broaker is used.

--- SubscriberTest.js
+++ SubscriberTest.js
@@ -1,10 +1,28 @@
 var mqtt = require('mqtt')
 
+var myArgs = process.argv.slice(2);
+switch (myArgs[0]) {
+    case 'mac':
+        var brokerAddress = 'mqtt://192.168.1.252';
+        console.log('Running on Mac.');
+        break;
+    case 'pi':
+        var brokerAddress = 'mqtt://192.168.1.251';
+        console.log('Running on Raspberry Pi.');
+        break;
+    default:
+        var brokerAddress = 'mqtt://192.168.1.252';
+        console.log('Running on Mac.');
+}
+
+var myArgs = process.argv.slice(2);
+
+
 var options = {
     username: 'still',
     password: Buffer.alloc(6, 'dillon'), // Passwords are buffers
 }  
-var client  = mqtt.connect('mqtt://192.168.1.252', options)
+var client  = mqtt.connect(brokerAddress, options)
 
 client.on('connect', function () {
     client.subscribe('still/columnTemp/STATUS10');
